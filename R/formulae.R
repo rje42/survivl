65f6a70d311921880a3ffc939f6b54d712431f1e
XiLinStats <xi.lin@stats.ox.ac.uk>
2024-10-17 10:39:12 +0100

    Fixed minor bug in curr_inputs

--- R/formulae.R
+++ R/formulae.R
@@ -203,10 +203,11 @@ curr_inputs <- function (formulas, pars, t, ordering, done, vars_t, kwd, start_a
     form_copY <- formulas[[4]][[LHS]]
     if (is.null(form_copY)) next
     pars_copY <- pars[[kwd]][[LHS]]
-    LHSs <- lhs(form_copY)
+
 
     j <- 1
     while (j <= length(form_copY)) {
+      LHSs <- lhs(form_copY)
       tmp <- mod_args(form_copY[[j]], pars_copY[[LHSs[j]]]$beta, t = t, modLHS = TRUE)
       form_copY[[j]] <- tmp$form
       pars_copY[[LHSs[j]]]$beta <- tmp$beta
@@ -214,7 +215,8 @@ curr_inputs <- function (formulas, pars, t, ordering, done, vars_t, kwd, start_a
     }
     formulas[[4]][[LHS]] <- form_copY
     formulas[[4]] <- formulas[[4]][!sapply(formulas[[4]], is.null)]
-    pars[[kwd]][[LHS]] <- pars_copY
+    pars[[kwd]][[LHS]] <- Filter(Negate(function(x) length(x) ==
+                                          0), pars_copY)
   }
 
   ## adjust parameter names
